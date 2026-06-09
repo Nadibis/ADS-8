1b04d01c76d97cf47fa353a44d8ba2f974e33984
Nadibis <nadia041007@gmail.com>
2026-06-09 16:24:18 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 // Copyright 2021 NNTU-CS
 #include "bst.h"
+#include <iostream>
+#include <string>
+#include <chrono>
+
+void makeTree(BST<std::string>& tree, const char* filename);
+void printFreq(BST<std::string>& tree);
 
 int main() {
+    BST<std::string> tree;
+    const char* filename = "war_peace.txt";
+    makeTree(tree, filename);
+    printFreq(tree);
     return 0;
 }
